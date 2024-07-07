d4579ebdb60173bcbfd1cbb91ab8c07813bc2266
huacai <ty101059@qq.com>
2024-07-07 23:24:38 +0800

    speed calibration v2 and add flightControl

---
+++ flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/DependInfo.cmake
@@ -0,0 +1,81 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/HC/Desktop/STM32-project/flightControl/source/Core/Startup/startup_stm32f411ceux.s" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Core/Startup/startup_stm32f411ceux.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "DEBUG"
+  "STM32F411xE"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/HC/Desktop/STM32-project/flightControl/source/Core/Src/gpio.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Core/Src/gpio.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Core/Src/main.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Core/Src/main.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Core/Src/stm32f4xx_hal_msp.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Core/Src/stm32f4xx_it.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Core/Src/syscalls.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Core/Src/syscalls.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Core/Src/sysmem.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Core/Src/sysmem.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Core/Src/system_stm32f4xx.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Core/Src/tim.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Core/Src/tim.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "D:/HC/Desktop/STM32-project/flightControl/source/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "D:/HC/Desktop/STM32-project/flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "DEBUG"
+  "STM32F411xE"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ flightControl/source/cmake-build-debug/CMakeFiles/source.elf.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/source.elf.dir/Core/Src/gpio.c.obj"
+  "CMakeFiles/source.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/source.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "CMakeFiles/source.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "CMakeFiles/source.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/source.elf.dir/Core/Src/sysmem.c.obj"
+  "CMakeFiles/source.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "CMakeFiles/source.elf.dir/Core/Src/tim.c.obj"
+  "CMakeFiles/source.elf.dir/Core/Startup/startup_stm32f411ceux.s.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "CMakeFiles/source.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "source.elf"
+  "source.elf.manifest"
+  "source.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/source.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
